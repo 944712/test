2b42a34306414be589dfd611ec4b6937b536ae30
jonghee <kjong7326@naver.com>
2017-10-27 00:00:03 +0900

    kjh/10.26/11:59

---
+++ app/src/main/java/com/example/user/test/AudiioActivity.java
@@ -0,0 +1,27 @@
+package com.example.user.test;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class AudiioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_audiio);
+    }
+    public void qr_info(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://me2.do/Gqi6KWiq"));
+        startActivity(intent);
+
+    }
+
+    public void qr_smart(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=kto.smarttour"));
+        startActivity(intent);
+
+    }
+}
