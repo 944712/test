baaedfd9f73571be32b9d3a052a69d15848cbaec
YUNA-KIM <jry_k@naver.com>
2017-10-30 04:52:24 +0900

    KYA
    KYA

--- app/src/main/java/com/example/user/test/SeeNearActivity.java
+++ app/src/main/java/com/example/user/test/SeeNearActivity.java
@@ -2,6 +2,8 @@ package com.example.user.test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SeeNearActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class SeeNearActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.see_near);
+
+        Button btn1 = (Button) findViewById(R.id.jonlimdongButton);
+        btn1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // Intent intent = new Intent(getApplicationContext(),) ;
+            }
+        });
     }
 }
