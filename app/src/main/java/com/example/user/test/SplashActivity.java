8ddbfc66dbfe2d4087c57638aa7d13e6468d1e9e
jonghee <kjong7326@naver.com>
2017-10-29 22:18:47 +0900

    kjh

--- app/src/main/java/com/example/user/test/SplashActivity.java
+++ app/src/main/java/com/example/user/test/SplashActivity.java
@@ -5,9 +5,6 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
-/**
- * Created by Administrator on 2017-10-26.
- */
 
 public class SplashActivity extends AppCompatActivity {
     @Override
