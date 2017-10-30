b75f05c307082d51cc7ff895290f8029984f1621
chaehyeon <eprtm22@naver.com>
2017-10-31 07:36:48 +0900

    10.31 07:36am kch

--- app/src/main/java/com/example/user/test/SplashActivity.java
+++ app/src/main/java/com/example/user/test/SplashActivity.java
@@ -5,15 +5,17 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+/**
+ * Created by USER on 2017-10-31.
+ */
 
 public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-
+        Intent x = new Intent(SplashActivity.this, MainActivity.class);
+        startActivity(x);
         finish();
     }
 }
